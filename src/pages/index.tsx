662f07ba77d900860c946e5b813056c9acdd323b
Dirivial <Alexander.Kadeby@gmail.com>
2022-09-04 13:16:03 +0200

    Moved trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
   return (
     <>
       <Head>
@@ -51,9 +48,6 @@ const Home: NextPage = () => {
               </div>
             </Link>
           </section>
-        </div>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
         </div>
       </main>
     </>
--- src/pages/workout/index.tsx
+++ src/pages/workout/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { trpc } from "../../utils/trpc";
 
 const WorkoutPage: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
   return (
     <>
       <Link href="/">Home</Link>
       <div>Gonna work out, are you?</div>
+      <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+        {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+      </div>
     </>
   );
 };
