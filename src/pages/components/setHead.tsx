d4e09fc4c348cb93760c984ee0d7463b17c40b83
Dirivial <Alexander.Kadeby@gmail.com>
2022-09-04 21:51:17 +0200

    Component for setting data seen in the tab

---
+++ src/pages/components/setHead.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const SetHead = () => {
+  return (
+    <Head>
+      <title>Fit.Dirivial</title>
+      <meta name="description" content="Fitness/workout app made by Dirivial" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default SetHead;
