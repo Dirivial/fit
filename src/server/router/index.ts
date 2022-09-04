6eda09364f673628f0b467de374191cc10b77fff
Dirivial <Alexander.Kadeby@gmail.com>
2022-09-04 21:52:26 +0200

    Created router for exercises

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,10 +4,13 @@ import superjson from "superjson";
 
 import { exampleRouter } from "./example";
 import { workoutRouter } from "./workout";
+import { exerciseRouter } from "./exercise";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter).merge("workout.", workoutRouter);
+  .merge("example.", exampleRouter)
+  .merge("workout.", workoutRouter)
+  .merge("exercise.", exerciseRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
