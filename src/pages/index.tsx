0ea0de4aa228e479e270db6c7450a533d58caf3e
Dirivial <Alexander.Kadeby@gmail.com>
2022-09-15 22:48:21 +0200

    Only display workout/analyze when logged in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,41 +21,50 @@ const Home: NextPage = () => {
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-400 via-violet-800 to-indigo-500">
           Fit.Dirivial
         </h1>
-        <p className="text-2xl text-gray-200">What would you like to do?</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          <section className="flex flex-col justify-center duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-            <Link href="/workout">
-              <div className="p-6 cursor-pointer flex-grow">
-                <h2 className="text-2xl justify-start font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-violet-700 to-red-600">
-                  Workout
-                </h2>
-                <hr />
-                <div className="p-2" />
-                <p className="text-sm font-semibold text-gray-200">
-                  Start working out!
-                </p>
-              </div>
-            </Link>
-          </section>
-          <section className="flex flex-col justify-center duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-            <Link href="/analysis">
-              <div className="p-6 cursor-pointer flex-grow">
-                <h2 className="text-2xl justify-start font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-green-500 to-violet-700">
-                  Analyze
-                </h2>
-                <hr />
-                <div className="p-2" />
-                <p className="text-sm font-semibold text-gray-200">
-                  Take a look at how you have progressed.
-                </p>
-              </div>
-            </Link>
-          </section>
-        </div>
+
         {session ? (
-          <p>
-            Hi, {session.user ? session.user.email : "some user"}
-            <button onClick={() => signOut()}>Sign out</button>
+          <>
+            <p className="text-2xl text-gray-200">What would you like to do?</p>
+            <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
+              <section className="flex flex-col justify-center duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+                <Link href="/workout">
+                  <div className="p-6 cursor-pointer flex-grow">
+                    <h2 className="text-2xl justify-start font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-violet-700 to-red-600">
+                      Workout
+                    </h2>
+                    <hr />
+                    <div className="p-2" />
+                    <p className="text-sm font-semibold text-gray-200">
+                      Start working out!
+                    </p>
+                  </div>
+                </Link>
+              </section>
+              <section className="flex flex-col justify-center duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+                <Link href="/analysis">
+                  <div className="p-6 cursor-pointer flex-grow">
+                    <h2 className="text-2xl justify-start font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-green-500 to-violet-700">
+                      Analyze
+                    </h2>
+                    <hr />
+                    <div className="p-2" />
+                    <p className="text-sm font-semibold text-gray-200">
+                      Take a look at how you have progressed.
+                    </p>
+                  </div>
+                </Link>
+              </section>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+        {session ? (
+          <p className="text-gray-200 mt-2">
+            Welcome, {session.user ? session.user.email : ""}.{" "}
+            <button onClick={() => signOut()} className="text-violet-400">
+              Sign out
+            </button>
           </p>
         ) : (
           <p>
