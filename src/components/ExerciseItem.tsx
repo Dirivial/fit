8c6821e765c7f463aa035c2edbd3c11105ab83ab
Dirivial <Alexander.Kadeby@gmail.com>
2022-09-26 20:03:20 +0200

    Styling changes

--- src/components/ExerciseItem.tsx
+++ src/components/ExerciseItem.tsx
@@ -130,118 +130,149 @@ const SetList = ({
     return val < 1000 ? (val > -1 ? val : 0) : 999;
   };
   return (
-    <section className="flex flex-col sm:flex-row justify-start rounded shadow-xl">
-      <div className="flex sm:flex-col flex-row justify-center gap-5 sm:gap-0 text-lg p-2 text-center text-gray-200">
-        <h3>Reps</h3>
-        <div className="p-2" />
-        <h3>Rest</h3>
-        <div className="p-2" />
-        <h3>Weight</h3>
-      </div>
-      <div ref={child} className="flex flex-col sm:flex-row overflow-x-scroll">
-        {sets.map((set, index) => {
-          return (
-            <div
-              key={index}
-              className="flex sm:flex-col flex-row justify-center gap-5 sm:gap-0 text-lg p-2 text-center text-gray-200"
-            >
-              <input
-                className="p-1 hover:cursor-default w-12 bg-transparent border-2 border-pink-700 rounded"
-                value={set.reps}
-                onChange={(e) => {
-                  let newSet = null;
+    <section className="flex flex-col sm:flex-row rounded shadow-xl w-fit">
+      <div className="flex-grow flex flex-col sm:flex-row justify-start">
+        <div className="flex sm:flex-col flex-row justify-center gap-5 sm:gap-0 text-lg p-2 text-center text-gray-200">
+          <h3>Reps</h3>
+          <div className="p-2" />
+          <h3>Rest</h3>
+          <div className="p-2" />
+          <h3>Weight</h3>
+        </div>
+        <div
+          ref={child}
+          className="flex flex-col sm:flex-row overflow-x-scroll"
+        >
+          {sets.map((set, index) => {
+            return (
+              <div
+                key={index}
+                className="flex sm:flex-col flex-row justify-center gap-5 sm:gap-0 text-lg p-2 text-center text-gray-200"
+              >
+                <input
+                  className="p-1 hover:cursor-default w-12 bg-transparent border-2 border-pink-700 rounded"
+                  value={set.reps}
+                  onChange={(e) => {
+                    let newSet = null;
 
-                  setSets((prev) => {
-                    const next = [...prev];
-                    const item = next[index];
-                    if (item) {
-                      item.reps = getValidNumber(e.target.value);
-                      next[index] = item;
-                      newSet = item;
-                    }
-                    return next;
-                  });
-                  if (newSet) updateSet(newSet, index);
-                }}
-              />
-              <div className="p-1" />
-              <input
-                className="p-1 hover:cursor-default w-12 bg-transparent border-2 border-pink-700 rounded"
-                value={set.rest}
-                onChange={(e) => {
-                  let newSet = null;
-                  setSets((prev) => {
-                    const next = [...prev];
-                    const item = next[index];
-                    if (item) {
-                      next[index]!.rest = getValidNumber(e.target.value);
-                      next[index] = item;
-                      newSet = item;
-                    }
-                    return next;
-                  });
-                  if (newSet) updateSet(newSet, index);
-                }}
-              />
-              <div className="p-1" />
-              <input
-                className="p-1 hover:cursor-default w-12 bg-transparent border-2 border-pink-700 rounded"
-                value={set.weight}
-                onChange={(e) => {
-                  let newSet = null;
-                  setSets((prev) => {
-                    const next = [...prev];
-                    newSet = next[index];
-                    if (newSet) {
-                      newSet.weight = getValidNumber(e.target.value);
-                      next[index] = newSet;
-                    }
-                    return next;
-                  });
-                  if (newSet) updateSet(newSet, index);
-                }}
-              />
-            </div>
-          );
-        })}
+                    setSets((prev) => {
+                      const next = [...prev];
+                      const item = next[index];
+                      if (item) {
+                        item.reps = getValidNumber(e.target.value);
+                        next[index] = item;
+                        newSet = item;
+                      }
+                      return next;
+                    });
+                    if (newSet) updateSet(newSet, index);
+                  }}
+                />
+                <div className="p-1" />
+                <input
+                  className="p-1 hover:cursor-default w-12 bg-transparent border-2 border-pink-700 rounded"
+                  value={set.rest}
+                  onChange={(e) => {
+                    let newSet = null;
+                    setSets((prev) => {
+                      const next = [...prev];
+                      const item = next[index];
+                      if (item) {
+                        next[index]!.rest = getValidNumber(e.target.value);
+                        next[index] = item;
+                        newSet = item;
+                      }
+                      return next;
+                    });
+                    if (newSet) updateSet(newSet, index);
+                  }}
+                />
+                <div className="p-1" />
+                <input
+                  className="p-1 hover:cursor-default w-12 bg-transparent border-2 border-pink-700 rounded"
+                  value={set.weight}
+                  onChange={(e) => {
+                    let newSet = null;
+                    setSets((prev) => {
+                      const next = [...prev];
+                      newSet = next[index];
+                      if (newSet) {
+                        newSet.weight = getValidNumber(e.target.value);
+                        next[index] = newSet;
+                      }
+                      return next;
+                    });
+                    if (newSet) updateSet(newSet, index);
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex sm:flex-col justify-evenly gap-2 p-2">
+          <button
+            onClick={() => {
+              const newSet: ExerciseSet = {
+                id: 0,
+                reps: 5,
+                rest: 60,
+                weight: 0,
+                exerciseId: null,
+                workoutExerciseId: exerciseId,
+              };
+
+              updateSet(newSet, sets.length);
+              setSets((prev) => {
+                return [...prev, newSet];
+              });
+            }}
+            className="border-2 rounded border-pink-700 text-gray-200 p-1 flex-grow flex justify-center items-center"
+          >
+            <FontAwesomeIcon icon={faPlus} className="w-6" />
+          </button>
+
+          <button
+            onClick={() => {
+              setSets((prev) => {
+                const sets = [...prev];
+                sets.pop();
+                return sets;
+              });
+              removeSet();
+            }}
+            className="border-2 rounded border-pink-700 text-gray-200 p-1 flex-grow flex justify-center items-center"
+          >
+            <FontAwesomeIcon icon={faMinus} className="w-6" />
+          </button>
+        </div>
       </div>
-      <div className="flex sm:flex-col justify-center sm:pb-0 pb-2">
-        <div className="p-1" />
+
+      <div className="flex sm:flex-col flex-row justify-evenly gap-2 text-lg p-2 text-center text-gray-200">
         <button
           onClick={() => {
-            const newSet: ExerciseSet = {
-              id: 0,
-              reps: 5,
-              rest: 60,
-              weight: 0,
-              exerciseId: null,
-              workoutExerciseId: exerciseId,
-            };
-
-            updateSet(newSet, sets.length);
-            setSets((prev) => {
-              return [...prev, newSet];
-            });
+            console.log("Save");
           }}
           className="border-2 rounded border-pink-700 text-gray-200 p-1 flex-grow flex justify-center items-center"
         >
-          <FontAwesomeIcon icon={faPlus} className="w-6" />
+          Save
         </button>
-        <div className="p-1" />
+
         <button
           onClick={() => {
-            setSets((prev) => {
-              const sets = [...prev];
-              sets.pop();
-              return sets;
-            });
-            removeSet();
+            console.log("Log");
           }}
           className="border-2 rounded border-pink-700 text-gray-200 p-1 flex-grow flex justify-center items-center"
         >
-          <FontAwesomeIcon icon={faMinus} className="w-6" />
+          Log
         </button>
-        <div className="p-1" />
+        <button
+          onClick={() => {
+            console.log("Delete");
+          }}
+          className="border-2 rounded border-pink-700 text-gray-200 p-1 flex-grow flex justify-center items-center"
+        >
+          Delete
+        </button>
       </div>
     </section>
   );
