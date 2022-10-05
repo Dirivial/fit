114ace81741d5874c7a11fa1d9c9d8548766da30
Dirivial <Alexander.Kadeby@gmail.com>
2022-10-05 23:42:22 +0200

    Added query with containing workout

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -1,18 +1,35 @@
 import { createRouter } from "./context";
 import { z } from "zod";
 
-export const user = createRouter().query("get", {
-  input: z.object({
-    email: z.string(),
-  }),
-  async resolve({ ctx, input }) {
-    console.log("Fetching users with email: ", input.email);
-    const user = await ctx.prisma.user.findFirst({
-      where: {
-        email: input.email,
-      },
-    });
-    console.log(user);
-    return user;
-  },
-});
+export const user = createRouter()
+  .query("get", {
+    input: z.object({
+      email: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const user = await ctx.prisma.user.findFirst({
+        where: {
+          email: input.email,
+        },
+      });
+      console.log(user);
+      return user;
+    },
+  })
+  .query("getWithWorkouts", {
+    input: z.object({
+      email: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const user = await ctx.prisma.user.findFirst({
+        where: {
+          email: input.email,
+        },
+        include: {
+          Workout: true,
+        },
+      });
+      console.log(user);
+      return user;
+    },
+  });
